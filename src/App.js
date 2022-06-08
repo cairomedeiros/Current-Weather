7ae5f737a33b28f6ea94918de7c478e57eb43708
cairomedeiros <cairomedeiros13@gmail.com>
2022-06-08 18:48:41 -0300

    css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import api from './api/api';
 import { useCallback, useEffect, useState } from 'react';
+import './style.css';
+
 
 function App() {
 
@@ -13,16 +15,27 @@ function App() {
   }, [])
 
   const handleSubmit = useCallback((e) => {
+    
     e.preventDefault();
     async function submit(){
+     
 
-   
+    const response = await api.get(`/weather?q=${city}&appid=b8342943dabdefa84d2a44b07fde9d13&units=metric&lang=pt_br`);
 
-    const response = await api.get(`/weather?q=${city}&appid=b8342943dabdefa84d2a44b07fde9d13&units=metric`);
-    const data = {
-      temperatura: response.data.main.temp_max,
+    const hasCidade = dt.find( cidade => cidade.idzada === city);
+
+    if(hasCidade){
+      throw new Error('Clima da cidade já está sendo mostrado!')
     }
 
+    const data = {
+      idzada: response.data.sys.id,
+      temperatura: response.data.main.temp,
+      tempmax: response.data.main.temp_max,
+      tempmin: response.data.main.temp_min,
+      weatherr: response.data.weather[0].description,
+    }
+    
     setDt([...dt, data]);
     setCity('');
 
@@ -37,11 +50,27 @@ function App() {
 
   return (
     <div className="App">
-     <div className="container-">
+     <div className="container">
 
         <h1>Previsão do tempo</h1>
 
-        <form onSubmit={handleSubmit}>
+        <div>
+          {dt.map(dados => (
+            <div className="weatherReport" key={dados.idzada}>
+              <p>Niterói, RJ - BR</p>
+              <h1>20º Nublado</h1>
+
+              <div className="infos">
+                <h2>16°^25°</h2>
+                <h2>Sensação 19°c</h2>
+                <h2>Vento 18km/h</h2>
+                <h2>Humidade 89%</h2>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <form className="forms" onSubmit={handleSubmit}>
 
           <input 
           type="text" 
@@ -49,20 +78,20 @@ function App() {
           value={city}
           onChange={inputChange}
           />
-          <button type="submit">Search</button>
+          <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
 
         </form>
 
-        <ul>
-          {dt.map(dados => (
-            <li key={dados.temperatura}>
-              <span>{dados.temperatura}</span>
-            </li>
-          ))}
-        </ul>
+        
         
 
      </div>
+
+     <div className="capitais">
+
+     </div>
+
+
     </div>
   );
 }
